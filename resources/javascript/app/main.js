07817d0a07ce95f6926a0e222d6d78453326ea39
Nikita Kozlov <1mr.njc@gmail.com>
2015-05-15 09:42:18 +0300

    added animation

--- resources/javascript/app/main.js
+++ resources/javascript/app/main.js
@@ -1,1 +1,8 @@
 var app = angular.module('myApp', ['ngAnimate', 'timer']);
+
+app.controller('PhotoBox', function ($scope) {
+    $scope.pictures = [
+        "resources/imgs/1.JPG",
+        "resources/imgs/2.JPG"
+    ];
+});

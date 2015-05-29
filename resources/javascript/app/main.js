7049974c29e7b459ddba0634f67ac725a0a0b497
Nikita Kozlov <1mr.njc@gmail.com>
2015-05-29 08:57:32 +0300

    jquery instead AngularJS

--- resources/javascript/app/main.js
+++ resources/javascript/app/main.js
@@ -1,25 +1,36 @@
-var app = angular.module('myApp', ['timer']);
-
-//dependencies: jquery, vegas
-app.directive('vegas', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            $(document).ready(function () {
-                $(".photoBox-container").vegas({
-                    slides: [
-                        {src: 'resources/imgs/1.JPG'},
-                        {src: 'resources/imgs/2.JPG'}
-                    ],
-                    transition: 'fade',
-                    timer: false,
-                    delay: 5000,
-                    overlay: "resources/imgs/vegas/overlays/01.png",
-                    cover: true
-                });
-            });
-        }
-    }
+$(document).ready(function () {
+    // vegas (slide show)
+    $(".photoBox-container").vegas({
+        slides: [
+            {src: 'resources/imgs/1.JPG'},
+            {src: 'resources/imgs/2.JPG'}
+        ],
+        transition: 'fade',
+        timer: false,
+        delay: 5000,
+        overlay: "resources/imgs/vegas/overlays/01.png",
+        cover: true
+    });
+    $('#clock').countdown('2015/07/12').on('update.countdown', function (event) {
+        var $this = $(this).html(event.strftime(''
+                + '<div class="countdown_section">'
+                + '<div class="countdown_amount">%D</div> '
+                + '<span>дней</span> '
+                + '</div>'
+                + '<div class="countdown_section">'
+                + '<div class="countdown_amount">%H</div> '
+                + '<span>часов</span> '
+                + '</div>'
+                + '<div class="countdown_section">'
+                + '<div class="countdown_amount">%M</div> '
+                + '<span>мин</span> '
+                + '</div>'
+                + '<div class="countdown_section">'
+                + '<div class="countdown_amount seconds">%S</div> '
+                + '<span>сек</span>'
+                + '</div>'
+        ));
+    });
 });
 
 
